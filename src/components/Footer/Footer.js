6c9902f695239661436a73ef2860d0fafbd68f93
Tevin <tevinward7@gmail.com>
2020-07-29 01:04:41 -0400

    added style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,14 +5,31 @@ import linkdIMg from "./img/linkedin.png"
 
 const Footer = () => {
     return(
-    <footer className="row">
+    <footer>
+        <div className="row">
 
-        <div className="col-12">
-            <ul>
-                <li><a href="https://www.linkedin.com/in/tevin-ward-bb1494150/" ><img src={linkdIMg} alt="github-link" height="30" width="30" /></a></li>
-                <li><a href="https://github.com/TevinWard7"><img src={gitImg} alt="github-link" height="30" width="30" /></a></li>
-            </ul>
-            <div>© 2020, Tevin Ward</div>
+        <div className="col-12" id="menu-outer">
+            <div class="table">
+                <ul id="horizontal-list">
+                    <li><a href="https://github.com/TevinWard7"><img src={gitImg} alt="github-link" height="30" width="30" /></a></li>
+                    <li><a href="https://www.linkedin.com/in/tevin-ward-bb1494150/" ><img src={linkdIMg} alt="github-link" height="30" width="30" /></a></li>
+                    <li><a href="https://github.com/TevinWard7"><img src={gitImg} alt="github-link" height="30" width="30" /></a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
+
+        <div className="row">
+            <div className="col-4">
+
+            </div>
+            <div className="col-4">
+               
+            </div>
+            <div className="col-4 bar">
+            © Tevin Ward
+            </div>
+
         </div>
 
     </footer>
