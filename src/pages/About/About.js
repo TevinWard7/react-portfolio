f35a691fe88d78146f7d1cc6e747d682395dedcd
Tevin <tevinward7@gmail.com>
2020-07-29 01:24:29 -0400

    added all pages

---
+++ src/pages/About/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div class="row myContainer">
+
+        <div class="col-12">
+            <p class="linked-pages-text">Web Developer based out of Goldsboro, North Carolina</p>
+
+            <a id="resume-link" href="content/tevin_ward_resume.pdf"> Resume </a>
+        </div>
+
+    </div>
+    )
+};
+
+export default About;
---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+
+    return(
+        <div class="myContainer">
+
+            <p class="linked-pages-text">tevinward7@gmail.com</p>
+
+        </div>
+    )
+};
+
+export default Contact;
