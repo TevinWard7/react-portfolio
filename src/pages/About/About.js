62632f3112e6c7f794e89e6c15acfb1e4e7528f3
Tevin <tevinward7@gmail.com>
2020-09-02 21:30:09 -0400

    edited about me page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
             
               <p class="linked-pages-text">
 
-                Hello, I'm Tevin Ward. A full-stack developer based out of Goldsboro, North Carolina with a coding certification from UNC Chapel Hill Bootcamp. I enjoy the journey and excitement of learning while building and find the challenge of programming to be be inherently rewarding.
+                Hello, I'm Tevin Ward. A full-stack developer based out of Goldsboro, North Carolina with a coding certification from UNC Chapel Hill Bootcamp. I enjoy the journey and excitement of learning while building. For all it's frustrations I find the challenge of programming to be be inherently rewarding, full of novelty, and in it's own right; a super power.
 
                 <br/>
                 <br/>
